8aa59faf3d9f5d0ed8d23752a453f01b6812eda0
sam37A <sahilsan30@gmail.com>
2022-01-16 14:56:17 -0800

    spelling is corrected

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
+//day 1 challenge
+
 // single line comment
 
 /*
-Muliline comment
+Multiline comment
 
 */
 console.log("suman Mainali")
 console.log("my favorite food:Chicken chilly")
+alert(1/0);
+let age;
+console.log(age);
+
+// daily challenge day 2
+
+//Using the .length property, console log the number of letters in “Supercalifragilisticexpialidocious”
+
+let letters = "Supercalifragilisticexpialidocious"
+console.log(letters.length);
+
+//Console log any number
+let num = 20;
+console.log(num);
+
+// Console log a BigInt, Hint: Use typeof to confirm your number and BigInt values
+let bigint1 = 78n;
+let bigint2 = 80n;
+console.log(bigint1 + bigint2);
+console.log(typeof bigint1);
+console.log(typeof bigint2);
+
+// take home challenge day 2
+
+alart("i am the alert box")
